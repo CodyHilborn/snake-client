c351d6f6187890698b1158fa31245377abc9188b
Cody Hilborn <codyhilborn11@gmail.com>
2023-01-18 19:32:37 +0000

    Moved the connection code from play file to client file, added module import/exports so that the play file will still run using the client file code

--- play.js
+++ play.js
@@ -1,22 +1,3 @@
-const { on } = require('events');
-const net = require('net');
-
-
-const connect = function() {
-  const conn = net.createConnection({
-    host: '10.0.2.15',
-    port: 50541,
-  });
-
-  conn.on('data', () => {
-    console.log('you ded cuz u idled');
-  });
-
-  conn.setEncoding('utf8');
-
-  return conn;
-};
-
-console.log('Connecting ...');
+const { connect } = require('./client');
 
 connect();
