0ac2ee687f5dbec24f3d11142d1798922ed7993f
Cody Hilborn <codyhilborn11@gmail.com>
2023-01-18 23:01:49 +0000

    General cleanup, added and removed some comments where necessary

--- client.js
+++ client.js
@@ -11,11 +11,6 @@ const connect = function() {
   conn.on('connect', () => {
     console.log('Successfully connected to the game server!');
     conn.write('Name: CJH');
-    // EXPERIMENTING WITH CONSECUTIVE COMMANDS
-    // conn.write('Move: up');
-    // setTimeout(() => {
-    //   conn.write('Move: left');
-    // }, 1000);
   });
 
   conn.on('data', () => {
--- input.js
+++ input.js
@@ -20,11 +20,12 @@ const setupInput = function(conn) {
 
 
 const handleUserInput = function(key) {
+  // TERMINAL ESCAPE KEY:
   if (key === '\u0003') {
     process.exit();
   }
 
-
+  // MOVE CONTROL KEYS:
   if (key === MOVE_UP_KEY) {
     connection.write('Move: up');
   }
@@ -38,9 +39,7 @@ const handleUserInput = function(key) {
     connection.write('Move: right');
   }
 
-
-
-
+  // CANNED MESSAGES:
   if (key === 'h') {
     connection.write('Say: Howdy');
   }
