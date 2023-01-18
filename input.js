43814bf80658c45c9d6d4724ddf3b02e43c64a68
Cody Hilborn <codyhilborn11@gmail.com>
2023-01-18 22:48:38 +0000

    Added a constants file and moved some static code into it, creating necessary module import/exports as needed

--- input.js
+++ input.js
@@ -1,3 +1,5 @@
+const { MOVE_UP_KEY, MOVE_LEFT_KEY, MOVE_DOWN_KEY, MOVE_RIGHT_KEY } = require('./constants');
+
 // setup interface to handle user input from stdin.
 
 let connection;
@@ -23,16 +25,16 @@ const handleUserInput = function(key) {
   }
 
 
-  if (key === 'w') {
+  if (key === MOVE_UP_KEY) {
     connection.write('Move: up');
   }
-  if (key === 'a') {
+  if (key === MOVE_LEFT_KEY) {
     connection.write('Move: left');
   }
-  if (key === 's') {
+  if (key === MOVE_DOWN_KEY) {
     connection.write('Move: down');
   }
-  if (key === 'd') {
+  if (key === MOVE_RIGHT_KEY) {
     connection.write('Move: right');
   }
 
@@ -40,10 +42,10 @@ const handleUserInput = function(key) {
 
 
   if (key === 'h') {
-    connection.write('Say: Howdy')
+    connection.write('Say: Howdy');
   }
   if (key === 'j') {
-    connection.write('Say: sssssss')
+    connection.write('Say: sssssss');
   }
 
 };
